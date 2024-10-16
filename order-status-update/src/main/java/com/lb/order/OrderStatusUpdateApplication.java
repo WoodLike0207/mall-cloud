a0a0cbdd0c35f6b937225778f711354a599c2813
WoodLike0207 <2416355572@qq.com>
2024-10-16 20:32:59 +0800

    创建订单超时取消相关服务

---
+++ order-status-update/src/main/java/com/lb/order/OrderStatusUpdateApplication.java
@@ -0,0 +1,17 @@
+package com.lb.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("com.lb.order.dao")
+public class OrderStatusUpdateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderStatusUpdateApplication.class, args);
+    }
+
+}
---
+++ order-timeout-cancel/src/test/java/com/lb/ordertimeoutcancel/OrderTimeoutCancelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lb.ordertimeoutcancel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderTimeoutCancelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
