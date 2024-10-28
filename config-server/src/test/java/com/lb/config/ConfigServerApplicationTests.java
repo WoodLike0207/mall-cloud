4d622dd03fd6651d8c2d6c5c5582925612b2c613
WoodLike0207 <2416355572@qq.com>
2024-10-28 21:31:01 +0800

    配置分布式配置中心

---
+++ config-server/src/test/java/com/lb/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lb.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
